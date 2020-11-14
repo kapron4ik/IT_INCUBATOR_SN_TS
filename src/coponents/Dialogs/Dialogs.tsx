db21b51b5ca5b3940c52a62fa7bf85ec5b61720f
kapron4ik server <kapron4ik@gmail.com>
2020-11-14 16:03:42 +0200

    delete test change commit

--- src/coponents/Dialogs/Dialogs.tsx
+++ src/coponents/Dialogs/Dialogs.tsx
@@ -22,7 +22,6 @@ const Dialogs: React.FC<DialogsPageType> = (props) => {
                 placeholder='Enter your message'/></div>
                 <div><button>Send</button></div>
             </div>
-            <div>sdfgsdfgsdfg</div>
         </div>
     )
 }
